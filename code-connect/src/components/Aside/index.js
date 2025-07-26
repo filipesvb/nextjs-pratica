c68d0a44a5064217e5895947dc197d4ed8783ebc
Filipe Vilas Boas <fvb.boas@gmail.com>
2025-07-26 17:27:30 -0300

    adicionei lupa na barra de pesquisa e mudei a logo

--- code-connect/src/components/Aside/index.js
+++ code-connect/src/components/Aside/index.js
@@ -1,13 +1,16 @@
 import Image from "next/image";
-import styles from "./aside.module.css"
+import styles from "./aside.module.css";
 
 import logo from "./Logo.svg";
+import Link from "next/link";
 
 export const Aside = () => {
-    return (
-        <aside className={styles.aside}>
-            {/* <img src="/Logo.svg" alt="Logo da CodeConnect" /> */}
-            <Image src={logo} alt="Logo da CodeConnect" />
-        </aside>
-    )
-}
+  return (
+    <aside className={styles.aside}>
+      {/* <img src="/Logo.svg" alt="Logo da CodeConnect" /> */}
+      <Link href="/">
+        <Image src={logo} alt="Logo da CodeConnect" />
+      </Link>
+    </aside>
+  );
+};
