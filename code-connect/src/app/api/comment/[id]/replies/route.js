9f9be7123e7129910ce58a766c654f2e0f6a2709
Filipe Vilas Bôas <59072469+filipesvb@users.noreply.github.com>
2025-07-30 19:40:50 +0000

    fix: mudei findFirst para findMany no route da API

--- code-connect/src/app/api/comment/[id]/replies/route.js
+++ code-connect/src/app/api/comment/[id]/replies/route.js
@@ -2,7 +2,7 @@ import db from "../../../../../../prisma/db";
 
 
 export async function GET (_request, { params }) {
-    const replies = await db.comment.findFirst({
+    const replies = await db.comment.findMany({
         where: {
             parentId: parseInt(params.id)
         },
